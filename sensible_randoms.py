a375d3193ecb536c43e5ad02caeb7370583c23ac
Ashwin Pillay <2016.ashwin.pillay@ves.ac.in>
2020-04-10 21:37:38 +0530

    -renamed auto_chord to auto_chord_song to avoid confusion while accessing -added notes_midi_converter to generate random MIDI chord objects of the specified scale

--- sensible_randoms.py
+++ sensible_randoms.py
@@ -7,5 +7,5 @@ def non_repeated_randoms():
     global history_list
     history_list = history_list[1:]
     if len(history_list) is 0:
-        history_list = random.sample(range(4), 2)
+        history_list = random.sample(range(4), 4)
     return history_list[0]
